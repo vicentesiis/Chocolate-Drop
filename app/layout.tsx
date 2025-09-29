579dfb26d28e6d25defbb6e942743063ed3a4384
Vicente Cantu <vice.00@hotmail.com>
2025-09-29 17:35:04 -0600

    New assets.

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
       "ChocolateDrop is a ecommerce plataform to sell chocolates online and the presentational website for the brand.",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "Chocolate Drop",
@@ -44,35 +44,11 @@ export const metadata: Metadata = {
   icons: [
     {
       rel: "icon",
-      url: "/favicon.ico",
+      url: "/logo.png",
     },
     {
       rel: "apple-touch-icon",
-      url: "/apple-touch-icon.png",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/favicon-32x32.png",
-      sizes: "32x32",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/favicon-16x16.png",
-      sizes: "16x16",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/android-chrome-192x192.png",
-      sizes: "192x192",
-    },
-    {
-      rel: "icon",
-      type: "image/png",
-      url: "/android-chrome-512x512.png",
-      sizes: "512x512",
+      url: "/logo.png",
     },
   ],
   robots: {
