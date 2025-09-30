08ea8e3cfaeb7ee0929b8682621f978a7a792e0a
Vicente Cantu <vice.00@hotmail.com>
2025-09-29 18:40:00 -0600

    lass biome configuration

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,32 +5,32 @@ import { Button } from "@/components/ui/button";
 
 const Hero = () => {
   return (
-    <div className="min-h-[calc(100vh-4rem)] w-full flex items-center justify-center overflow-hidden border-b border-accent">
-      <div className="max-w-(--breakpoint-xl) w-full flex flex-col lg:flex-row mx-auto items-center justify-between gap-y-14 gap-x-10 px-6 py-12 lg:py-0">
+    <div className="flex min-h-[calc(100vh-4rem)] w-full items-center justify-center overflow-hidden border-accent border-b">
+      <div className="mx-auto flex w-full max-w-(--breakpoint-xl) flex-col items-center justify-between gap-x-10 gap-y-14 px-6 py-12 lg:flex-row lg:py-0">
         <div className="max-w-xl">
-          <Badge className="rounded-full py-1 border-none">Just released v1.0.0</Badge>
-          <h1 className="mt-6 max-w-[20ch] text-3xl xs:text-4xl sm:text-5xl lg:text-[2.75rem] xl:text-5xl font-bold leading-[1.2]! tracking-tight">
+          <Badge className="rounded-full border-none py-1">Just released v1.0.0</Badge>
+          <h1 className="mt-6 max-w-[20ch] font-bold text-3xl xs:text-4xl leading-[1.2]! tracking-tight sm:text-5xl lg:text-[2.75rem] xl:text-5xl">
             Customized Shadcn UI Blocks & Components
           </h1>
           <p className="mt-6 max-w-[60ch] xs:text-lg">
             Explore a collection of Shadcn UI blocks and components, ready to preview and copy.
             Streamline your development workflow with easy-to-implement examples.
           </p>
-          <div className="mt-12 flex flex-col sm:flex-row items-center gap-4">
-            <Button size="lg" className="w-full sm:w-auto rounded-full text-base">
+          <div className="mt-12 flex flex-col items-center gap-4 sm:flex-row">
+            <Button size="lg" className="w-full rounded-full text-base sm:w-auto">
               Get Started <ArrowUpRight className="h-5! w-5!" />
             </Button>
             <Button
               variant="outline"
               size="lg"
-              className="w-full sm:w-auto rounded-full text-base shadow-none"
+              className="w-full rounded-full text-base shadow-none sm:w-auto"
             >
               <CirclePlay className="h-5! w-5!" /> Watch Demo
             </Button>
           </div>
         </div>
-        <div className="relative lg:max-w-lg xl:max-w-xl w-full bg-accent rounded-xl aspect-square">
-          <Image src="/hero.jpg" fill alt="" className="object-cover rounded-xl" />
+        <div className="relative aspect-square w-full rounded-xl bg-accent lg:max-w-lg xl:max-w-xl">
+          <Image src="/hero.jpg" fill alt="" className="rounded-xl object-cover" />
         </div>
       </div>
     </div>
